970b967dc2f52a4d3bb1990d9ad6fc7866ec8e2a
racso69 <50759890+racso69@users.noreply.github.com>
2026-05-27 23:06:05 -0300

    Update Conexion.java

--- src/main/java/com/acquadimare/conexion/Conexion.java
+++ src/main/java/com/acquadimare/conexion/Conexion.java
@@ -5,21 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    // Si tu puerto es el estándar es 5432, si lo cambiaste usa 5433
-    private static final String URL = "jdbc:postgresql://localhost:5432/acquadimare_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "696986"; // <-- Coloca tu clave aquí
     
-    private static Connection conexion = null;
-
     public static Connection getConexion() {
+        Connection conexion = null;
         try {
-            if (conexion == null || conexion.isClosed()) {
-                // Registramos el driver de PostgreSQL
-                Class.forName("org.postgresql.Driver");
-                conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-                System.out.println("¡CONEXIÓN EXITOSA A ACQUA DI MARE DB!");
+            // Buscamos las variables de entorno de Render
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASS");
+            
+            // Si no existen (estás en tu PC), usamos los valores por defecto
+            if (url == null) {
+                url = "jdbc:postgresql://localhost:5432/acquadimare_db";
+                user = "postgres";
+                password = "696986";
             }
+            
+            Class.forName("org.postgresql.Driver");
+            conexion = DriverManager.getConnection(url, user, password);
+            System.out.println("¡CONEXIÓN EXITOSA!");
+            
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("ERROR DE CONEXIÓN: " + e.getMessage());
         }
